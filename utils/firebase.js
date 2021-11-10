7422f3aa8ca6492b029ddb1fe2571dbff4e18fca
ata parvin ghods <ataparvinghods@gmail.com>
2021-11-10 21:47:51 +0330

    Added basic stack navigation

---
+++ utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDukVzTKgeetFdri8iAtdZkQtx-m0iNaYg",
+    authDomain: "signal-clone-599aa.firebaseapp.com",
+    projectId: "signal-clone-599aa",
+    storageBucket: "signal-clone-599aa.appspot.com",
+    messagingSenderId: "661764021294",
+    appId: "1:661764021294:web:7af64833dc7029d3b4d3bf",
+}
