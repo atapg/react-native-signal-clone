82c2d026fe4ad15e80e18332636da7923a3dee8b
ata parvin ghods <ataparvinghods@gmail.com>
2021-11-12 19:14:48 +0330

    Added refresh on pulling down in Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,11 @@
 import React from "react"
-import { ScrollView, StyleSheet, Text, View } from "react-native"
+import {
+    RefreshControl,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native"
 import { Button, FAB } from "react-native-elements"
 import Message from "../components/chat/Message"
 import Container from "../components/generals/Container"
@@ -27,12 +33,51 @@ const users = [
         image: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/image_data/file/103058/s960_Minister_Donelan.jpeg",
         lastChat: "hardasan",
     },
+    {
+        username: "Ata parvin ghods",
+        image: "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
+        lastChat: "Hello boy how are you?",
+    },
+    {
+        username: "Zibaye xufta",
+        image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        lastChat: "how are you?",
+    },
+    {
+        username: "Adnan",
+        image: "https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg",
+        lastChat: "gadaaaa?",
+    },
+    {
+        username: "Ajabaa",
+        image: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/image_data/file/103058/s960_Minister_Donelan.jpeg",
+        lastChat: "hardasan",
+    },
 ]
 
+const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout))
+}
+
 const Home = ({ navigation }) => {
+    const [refreshing, setRefreshing] = React.useState(false)
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true)
+        wait(2000).then(() => setRefreshing(false))
+    }, [])
+
     return (
         <>
-            <ScrollView style={{ backgroundColor: "white" }}>
+            <ScrollView
+                style={{ backgroundColor: "white" }}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+            >
                 {users.map((user, index) => (
                     <Message
                         key={index}
@@ -43,13 +88,13 @@ const Home = ({ navigation }) => {
                     />
                 ))}
             </ScrollView>
-            <View style={styles.floatingBtn}>
+            {/* <View style={styles.floatingBtn}>
                 <Button
                     title="New Chat"
                     size="large"
                     color={Colors.primaryColor}
                 ></Button>
-            </View>
+            </View> */}
         </>
     )
 }
