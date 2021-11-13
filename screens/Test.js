61a7920d065fd7fa60812e0818d02e4092418bc8
ata parvin ghods <ataparvinghods@gmail.com>
2021-11-13 04:40:05 +0330

    Added flat list

--- screens/Test.js
+++ screens/Test.js
@@ -1,22 +1,60 @@
 import React from "react"
 import {
+    FlatList,
     Image,
     ImageBackground,
     ScrollView,
     StyleSheet,
     Text,
+    TouchableOpacity,
     View,
 } from "react-native"
 import Logo from "../components/test/Logo"
-import { BottomNavigation } from "react-native-paper"
+import { BottomNavigation, Button } from "react-native-paper"
 import Colors from "../utils/Colors"
 import Swiper from "react-native-swiper"
+import Carousel from "react-native-anchor-carousel"
+import { Rating } from "react-native-elements"
+import { Ionicons } from "@expo/vector-icons"
 
 const MusicRoute = () => <></>
 
 const AlbumsRoute = () => <></>
 
 const RecentsRoute = () => <></>
+
+const bestTeachers = [
+    {
+        name: "طناز صمدی",
+        image: "https://www.edarabia.com/wp-content/uploads/2015/11/7-ways-to-become-the-person-everyone-respects.jpg",
+        rating: 5,
+        field: "شیمی",
+    },
+    {
+        name: "سیما رستگار",
+        image: "https://www.harleytherapy.co.uk/counselling/wp-content/uploads/16297800391_5c6e812832.jpg",
+        rating: 1,
+        field: "فیزیک",
+    },
+    {
+        name: "بهرام ایزدی",
+        image: "https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg",
+        rating: 4,
+        field: "انگلیسی",
+    },
+    {
+        name: "ایرج زمانی",
+        image: "https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg",
+        rating: 3,
+        field: "عربی",
+    },
+    {
+        name: "پرستو کامروا",
+        image: "https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg",
+        rating: 2,
+        field: "ادبیات",
+    },
+]
 
 const Test = () => {
     const [index, setIndex] = React.useState(0)
@@ -41,28 +79,101 @@ const Test = () => {
     })
 
     return (
-        <View style={{ flex: 1 }}>
-            <View style={styles.swiperContainer}>
-                <Swiper
-                    autoplay={true}
-                    showsPagination={false}
-                    style={styles.swiper}
-                >
-                    {sliderImages.map((img, index) => (
-                        <View key={index} style={{ height: 200 }}>
-                            <Image
-                                resizeMode="cover"
-                                source={{
-                                    uri: img,
-                                }}
-                                height={200}
-                                style={{ flex: 1 }}
-                                style={styles.image}
-                            />
-                        </View>
-                    ))}
-                </Swiper>
-            </View>
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.wrapper}>
+                    <View style={styles.swiperContainer}>
+                        <Swiper
+                            autoplay={true}
+                            showsPagination={false}
+                            style={styles.swiper}
+                        >
+                            {sliderImages.map((img, index) => (
+                                <View key={index} style={{ height: 200 }}>
+                                    <Image
+                                        resizeMode="cover"
+                                        source={{
+                                            uri: img,
+                                        }}
+                                        height={200}
+                                        style={{ flex: 1 }}
+                                        style={styles.image}
+                                    />
+                                </View>
+                            ))}
+                        </Swiper>
+                    </View>
+                    <View style={styles.teachersSection}>
+                        <Button
+                            mode="outlined"
+                            onPress={() => console.log("clicked")}
+                            color={Colors.primaryColor}
+                            labelStyle={{ fontWeight: "bold" }}
+                        >
+                            مشاهده همه
+                        </Button>
+                        <Text style={{ fontSize: 25, fontWeight: "bold" }}>
+                            مدرسان برتر
+                        </Text>
+                    </View>
+                    <View style={styles.teachersSlider}>
+                        <FlatList
+                            data={bestTeachers}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity
+                                    style={{ backgroundColor: "#fff" }}
+                                >
+                                    <View
+                                        style={styles.bestTeachersContainer}
+                                        key={item.name}
+                                    >
+                                        <View style={styles.overlay}></View>
+                                        <Image
+                                            resizeMode="cover"
+                                            source={{
+                                                uri: item.image,
+                                            }}
+                                            style={{ flex: 1 }}
+                                        />
+                                        <View style={{ zIndex: 10 }}>
+                                            <Text style={styles.teacherName}>
+                                                {item.name}
+                                            </Text>
+                                            <View
+                                                style={styles.ratingContainer}
+                                            >
+                                                {Array(Math.round(item.rating))
+                                                    .fill()
+                                                    .map((_, i) => (
+                                                        <Ionicons
+                                                            name="star"
+                                                            size={20}
+                                                            color="gold"
+                                                        />
+                                                    ))}
+                                            </View>
+                                        </View>
+                                    </View>
+                                </TouchableOpacity>
+                            )}
+                            horizontal={true}
+                        />
+                    </View>
+                    <View style={styles.teachersSection}>
+                        <Button
+                            mode="outlined"
+                            onPress={() => console.log("clicked")}
+                            color={Colors.primaryColor}
+                            labelStyle={{ fontWeight: "bold" }}
+                        >
+                            مشاهده همه
+                        </Button>
+                        <Text style={{ fontSize: 25, fontWeight: "bold" }}>
+                            یادت نره!
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
             <View style={[styles.bottomNav, styles.bottomNavShadow]}>
                 <BottomNavigation
                     navigationState={{ index, routes }}
@@ -81,10 +192,19 @@ const Test = () => {
 export default Test
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingBottom: 60,
+    },
+    wrapper: {
+        padding: 20,
+    },
     swiperContainer: {
         flex: 1,
-        padding: 20,
         borderRadius: 30,
+        marginBottom: 30,
+        borderRadius: 20,
+        overflow: "hidden",
     },
     swiper: {
         height: 200,
@@ -118,5 +238,45 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.58,
         shadowRadius: 16.0,
         elevation: 24,
+    },
+    teachersSection: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 30,
+    },
+    teachersSlider: {
+        height: 150,
+        marginBottom: 30,
+        marginHorizontal: -5,
+    },
+    bestTeachersContainer: {
+        height: "100%",
+        width: 150,
+        marginHorizontal: 5,
+        borderRadius: 15,
+        overflow: "hidden",
+        // position: "relative",
+    },
+    teacherName: {
+        position: "absolute",
+        bottom: 30,
+        right: 15,
+        color: "white",
+        fontSize: 20,
+    },
+    overlay: {
+        backgroundColor: "rgba(0,0,0, 0.4)",
+        position: "absolute",
+        right: 0,
+        left: 0,
+        bottom: 0,
+        top: 0,
+        zIndex: 1,
+    },
+    ratingContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 15,
+        flexDirection: "row",
     },
 })
